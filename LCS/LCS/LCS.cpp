3115f9d6442fb0c2b62fd96212b6cb2b73416fec
Kirill Isaev <isaevtjsg@mail.ru>
2025-10-12 16:36:08 +0300

    LCS, longest Palindrome

---
+++ LCS/LCS/LCS.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+struct Answer {
+    short result;
+    std::stack<short> indisex1;
+    std::stack<short> indisex2;
+    Answer() {
+        this->result = 0;
+        this->indisex1 = std::stack<short>();
+        this->indisex2 = std::stack<short>();
+    }
+    Answer(short result_): result(result_) {}
+    Answer(short result_, std::stack<short> indisex1_, std::stack<short> indisex2_): result(result_), indisex1(indisex1_), indisex2(indisex2_){}
+};
+
+short max(short a, short b) {
+    return a > b ? a : b;
+}
+
+Answer maxLen(std::vector<short> &arr1, std::vector<short> &arr2) {
+    if (arr1.size() > 1) {
+        Answer answer;
+        short** resultsMatr = new short* [arr1.size() + 1];
+        for (size_t i = 0; i < arr1.size() + 1; i++) {
+            resultsMatr[i] = new short[arr2.size() + 1];
+        }
+        for (size_t i = 0; i < arr1.size() + 1; i++) 
+            resultsMatr[i][0] = 0;
+        for (size_t i = 0; i < arr2.size() + 1; i++)
+            resultsMatr[0][i] = 0;
+        for (size_t i = 1; i < arr1.size() + 1; i++) {
+            for (size_t j = 1; j < arr2.size() + 1; j++) {
+                if (arr1[i - 1] == arr2[j - 1])
+                    resultsMatr[i][j] = resultsMatr[i - 1][j - 1] + 1;
+                else 
+                    resultsMatr[i][j] = max(resultsMatr[i][j - 1], resultsMatr[i - 1][j]);
+            }
+        }
+        size_t i = arr1.size();
+        size_t j = arr2.size();
+        answer.result = resultsMatr[i][j];
+        while (i > 0 && j > 0) {
+            if (arr1[i - 1] == arr2[j - 1]) {
+                answer.indisex1.push(i - 1);
+                answer.indisex2.push(j - 1);
+                i--;
+                j--;
+            }
+            else if (resultsMatr[i][j] == resultsMatr[i - 1][j] ) i--;
+            else j--;
+        }
+        for (int i = 0; i < arr1.size() + 1; i++)
+            delete[] resultsMatr[i];
+        delete[] resultsMatr;
+        return answer;
+    }
+    else if (arr1.size() == 1) return arr1[0] == arr2[0] ? Answer(1, std::stack<short>({0}), std::stack<short>({0})) : Answer(0);
+    else return Answer(0);
+}
+
+int main()
+{
+    short lenSequence;
+    std::cin >> lenSequence;
+    std::vector<short> arr1;
+    std::vector<short> arr2;
+    short temp = 0;
+    for (size_t i = 0; i < lenSequence; i++) {
+        std::cin >> temp;
+        arr1.push_back(temp);
+    }
+    for (size_t i = 0; i < lenSequence; i++) {
+        std::cin >> temp;
+        arr2.push_back(temp);
+    }
+    Answer answer = maxLen(arr1, arr2); 
+    int arrSize = answer.indisex1.size();
+    std::cout << answer.result;
+    if(!answer.indisex1.empty()){
+        std::cout << "\n";
+        for (int i = 0; i < arrSize; i++) {
+            if (i != arrSize - 1) std::cout << answer.indisex1.top() << " ";
+            else std::cout << answer.indisex1.top();
+            answer.indisex1.pop();
+        }
+        std::cout << "\n";
+        for (int i = 0; i < arrSize; i++) {
+            if (i != arrSize - 1) std::cout << answer.indisex2.top() << " ";
+            else std::cout << answer.indisex2.top();
+            answer.indisex2.pop();
+        }
+    }
+}
---
+++ Palindrome/Palindrome/Palindrome.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+struct Pal {
+    size_t size;
+    std::string str;
+    Pal(size_t size_) : size(size_) {
+        this->str = "";
+    }
+    Pal(size_t size_, std::string str_): size(size_), str(str_){}
+    Pal() {
+        this->size = 0;
+        this->str = "";
+    }
+};
+
+size_t max(size_t a, size_t b) {
+    return a > b ? a : b;
+}
+
+Pal maxPal(std::string str) {
+    size_t size1 = str.length();
+    if (size1 > 2) {
+        Pal pal;
+        short** resultsMatr = new short* [size1];
+        for (size_t i = 0; i < size1; i++)
+            resultsMatr[i] = new short[size1];
+        for (size_t i = 0; i < size1; i++)
+            for (size_t j = 0; j < size1; j++)
+                resultsMatr[i][j] = 0;
+        for (size_t i = 0; i < size1; i++)
+            resultsMatr[i][i] = 1;
+        for (size_t len = 2; len <= size1; ++len) {
+            for (size_t i = 0; i <= size1 - len; ++i) {
+                size_t j = len + i - 1;
+                if (str[i] == str[j])
+                    if (len == 2)
+                        resultsMatr[i][j] = 2;
+                    else 
+                        resultsMatr[i][j] = resultsMatr[i + 1][j - 1] + 2;
+                else
+                    resultsMatr[i][j] = max(resultsMatr[i + 1][j], resultsMatr[i][j - 1]);
+            }
+        }
+        size_t i = 0;
+        size_t j = size1 - 1;
+        std::vector<char> palindrom;
+        while (i <= j) {
+            if (str[i] == str[j]) {
+                palindrom.push_back(str[i]);
+                i++;
+                j--;
+            }
+            else {
+                if (resultsMatr[i + 1][j] == resultsMatr[i][j] && i + 1 < size1) i++;
+                else j--;
+            }
+        }
+        std::string result;
+        if (!palindrom.empty()) {
+            std::string right(palindrom.rbegin(), palindrom.rend());
+            std::string left(palindrom.begin(), palindrom.end());
+            if (resultsMatr[0][size1 - 1] % 2 == 0)
+                result = left + right;
+            else {
+                right.erase(0, 1);
+                result = left + right;
+            }
+
+        }
+        pal.size = resultsMatr[0][size1 - 1];
+        pal.str = result;
+        for (size_t i = 0; i < size1; i++)
+            delete[] resultsMatr[i];
+        delete[] resultsMatr;
+        return pal;
+    }
+    else if (size1 == 2)
+        if (str[0] == str[1]) return Pal(2, str);
+        else return Pal(1, str.erase(1));
+    else if (size1 == 1) return Pal(1, str);
+    else return Pal(0);
+}
+
+int main()
+{
+    std::string string;
+    std::ifstream in("input.txt");
+    in >> string;
+    in.close();
+    Pal pal = maxPal(string);
+    std::ofstream out("output.txt");
+    out << pal.size << "\n";
+    out << pal.str;
+    out.close();
+}
