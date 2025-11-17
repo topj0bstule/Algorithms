ca2343569a8659fa9d79024ee2bee46ff2045508
Kirill Isaev <topj0bstule@gmail.com>
2025-11-17 14:37:52 +0300

    hash table

---
+++ hashTable/hashTable/hashTable.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <fstream>
+
+int* hashTable(int nums[], short numsSize, short c, short hashTableSize) {
+	int* hashTable = new int[hashTableSize];
+	short temp = 0;
+	bool exist = false;
+	for (size_t i = 0; i < hashTableSize; i++) hashTable[i] = -1;
+	for (size_t i = 0; i < numsSize; i++) {
+		for (size_t j = 0; j < numsSize; j++) {
+			temp = ((nums[i] % hashTableSize) + c * j) % hashTableSize;
+			if (hashTable[temp] == nums[i]) {
+				exist = true;
+				break;
+			}
+			if (hashTable[temp] == -1) break;
+		}
+		if (!exist) {
+			for (size_t j = 0; j < numsSize; j++) {
+				temp = ((nums[i] % hashTableSize) + c * j) % hashTableSize;
+				if (hashTable[temp] == -1) {
+					hashTable[temp] = nums[i];
+					break;
+				}
+			}
+		}
+		exist = false;
+	}
+	return hashTable;
+}
+
+int main() {
+	short m = 0, c = 0, n = 0;
+
+	std::ifstream in("input.txt");
+	if (!in.is_open()) {
+		std::cerr << "Failed opening file.\n";
+		return -1;
+	}
+	in >> m >> c >> n;
+	int* nums = new int[n];
+	for (size_t i = 0; i < n; i++) in >> nums[i];
+	in.close();
+
+	int* result = hashTable(nums, n, c, m);
+
+	std::ofstream out("output.txt");
+	if (!out.is_open()) {
+		std::cerr << "Failed opening file.\n";
+		return -1;
+	}
+
+	for (size_t i = 0; i < m; i++) out << result[i] << " ";
+	out.close();
+
+	delete[] result;
+
+	return 0;
+}
